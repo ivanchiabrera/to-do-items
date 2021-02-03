90242a9e0246abd415a0163a23459d31aa64652a
ivanchiabrera <44534420+ivanchiabrera@users.noreply.github.com>
2021-02-03 14:36:43 -0300

    new script: run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ./frontend
+npm i
+ng build --prod
+mv ./dist/to-do-list ../backend/dist
+cd ../backend
+touch .env
+echo "URL_MONGO=mongodb://localhost:27017/to-do-list" >> .env | echo "BCRYPT_SALT_ROUNDS=12" >> .env | echo "JWT_TOKEN=!#!k5&$&/a446gJHF" >> .env
+npm install
+npm start
